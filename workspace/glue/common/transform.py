68b4abb0c0edfe80afd7f793efac72471c8aa5d1
YANG YANG <SixGod2019@outlook.com>
2023-03-10 10:28:16 +0800

    更新数据源和transform 接口

--- workspace/glue/common/transform.py
+++ workspace/glue/common/transform.py
@@ -4,3 +4,49 @@
 @Date : 2023/3/9 22:23
 """
 
+# -*- coding: utf-8 -*-
+"""
+@Author : YANG YANG
+@Date : 2023/3/9 22:23
+"""
+import abc
+import random
+
+
+# 生成 Glue Transform代码
+
+class TransformInterface(metaclass=abc.ABCMeta):
+    @abc.abstractmethod
+    def transform(self, sql_query):
+        pass
+
+    @abc.abstractmethod
+    def sql_query(self):
+        pass
+
+
+class TransformGenerator(TransformInterface):
+    def __init__(self, sql_path=None, datasource_node=()):
+        """
+        :param sql_path:
+        :param datasource_node: tuple 参数传入不定个数的datasource node
+        """
+        self.sql_path = sql_path
+        self.datasource_node = datasource_node
+
+    def transform(self, sql_query):
+        """
+        :param sql_query: sql_query 方法返回的sql字符集
+        :return:
+        """
+        pass
+
+    def sql_query(self):
+        """
+        从sql文件读取sql内容，并返回sql内容，要求格式不丢失
+        :return:
+        """
+        pass
+
+# 调用方法
+# source_ctx, source = generate_datasource_interface(CsvDatasource(database='devops', table_name='user_csv'))
