acfc958692a4d3c340ae6895cda1463b30c95860
YANG YANG <SixGod2019@outlook.com>
2023-03-10 11:26:50 +0800

    更新数据源和transform 接口

--- workspace/glue/common/source.py
+++ workspace/glue/common/source.py
@@ -35,6 +35,7 @@ class CsvDatasource(DatasourceInterface):
     table_name="{table_name}",
     transformation_ctx="{transformation_ctx}",
 )'''.format(database=self.database, table_name=self.table_name, transformation_ctx=self.transformation_ctx)
+        print(comment + sql)
         return self.transformation_ctx, comment + sql
 
 
