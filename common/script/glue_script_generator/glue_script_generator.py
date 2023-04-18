c01363b1fc6100ec6eccf72cb13f6291bf311da7
fangkui <colonel11@126.com>
2023-04-18 16:44:09 +0800

    update glue_script_generator.py main function

--- common/script/glue_script_generator/glue_script_generator.py
+++ common/script/glue_script_generator/glue_script_generator.py
@@ -86,6 +86,9 @@ if __name__ == '__main__':
     # gsg = GlueScriptGenerate('devops', u_unix, datetargetpath, out_py_path)
     # print(gsg)
     #print(gsg.get_script())
-
+    print(sys.argv[0])
+    print(sys.argv[1])
+    print(sys.argv[2])
+    print(sys.argv[3])
     gsg = GlueScriptGenerate(sys.argv[0], sys.argv[1], sys.argv[2], sys.argv[3])
     gsg.get_script()
