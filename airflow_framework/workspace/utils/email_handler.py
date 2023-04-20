97e2c829e469d473bad7c0b6185948842190b930
david.wu <958144600@qq.com>
2023-04-20 15:40:13 +0800

    提交消息发送SNS

--- airflow_framework/workspace/utils/email_handler.py
+++ airflow_framework/workspace/utils/email_handler.py
@@ -1,6 +1,8 @@
 import boto3
 from botocore.exceptions import ClientError
+import logging
 
+logger = logging.getLogger(__name__)
 # -*- coding: utf-8 -*-
 """
 @Author : YANG YANG
@@ -69,9 +71,42 @@ class EmailHandler:
             )
         # Display an error if something goes wrong.
         except ClientError as e:
-            print(e.response['Error']['Message'])
+            logger.exception(
+                "Couldn't send email : '%s'", e.response['Error']['Message'])
+            raise
         else:
-            print("Email sent! Message ID:"),
-            print(response['MessageId'])
+            logger.info("Email sent! Message ID: '%s'", response['MessageId'])
             return True
         return False
+
+    def send_email_sns(self, subject, body_test):
+        """
+        通过aws sns 发送邮件
+        :param subject: 标题
+        :param body_test: 正文
+        :return: True/False
+        """
+        AWS_REGION = "ap-northeast-1"
+        ACCESS_KEY = 'AKIAQJ4XUNJFYRLBDZYV'
+        SECRET_KEY = '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'
+        # Create a new SES resource and specify a region.
+        sns_client = boto3.client('sns', region_name=AWS_REGION, aws_access_key_id=ACCESS_KEY,
+                                  aws_secret_access_key=SECRET_KEY)
+
+        MY_SNS_TOPIC_ARN = 'arn:aws:sns:ap-northeast-1:021255973451:email'
+        try:
+            response = sns_client.publish(
+                TopicArn=MY_SNS_TOPIC_ARN,
+                Message=body_test,
+                Subject=subject
+            )
+            logger.info(response)
+        except ClientError as e:
+            logger.exception(
+                "Couldn't send email : '%s'", e.response['Error']['Message'])
+            raise
+        else:
+            logger.info("Email sent! Message ID: '%s'", response['MessageId'])
+            return True
+        return False
+
