cc8a7f964bbccf9a06cefc2dfe41d2ae834e6a95
zhenzhen <2450780537@qq.com>
2023-04-21 11:38:34 +0800

    add para base_url

--- airflow_framework/workspace/module/dependency-check-byapi.py
+++ airflow_framework/workspace/module/dependency-check-byapi.py
@@ -1,23 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+@Author : Liu Zhu
+@Date : 2023/4/21 11:31
+"""
 import requests
 import time
+import logging
+
+logging.basicConfig(level=logging.INFO)
+
 class MyDependencyChecker:
-    def __init__(self, task_id, dag_id, dag_run_id, waiting_time=60):
+    """
+    依赖任务检查器
+    :param task_id: 任务ID
+    :param dag_id: DAG ID
+    :param dag_run_id: DAG运行ID
+    :param waiting_time: 等待时间
+    :return: True/False
+    """
+    def __init__(self, task_id, dag_id, dag_run_id, waiting_time=60, base_url="http://43.143.250.12:8080"):
         self.task_id = task_id
         self.dag_id = dag_id
         self.dag_run_id = dag_run_id
         self.waiting_time = waiting_time
+        self.base_url = base_url
 
     def get_task_status(self):
-        # http://43.143.250.12:8080/api/v1/dags/{dag_id}/dagRuns/{dag_run_id}/taskInstances
         # https://airflow.apache.org/docs/apache-airflow/stable/stable-rest-api-ref.html#operation/get_task_instances
         # https://airflow.apache.org/api/v1/dags/{dag_id}/dagRuns/{dag_run_id}/taskInstances/{task_id}
-        job_status_api_url = "http://43.143.250.12:8080/api/v1/dags/{dag_id}/dagRuns/{dag_run_id}/taskInstances/{task_id}"
-        job_status_api_url = job_status_api_url.format(dag_id=self.dag_id, dag_run_id=self.dag_run_id, task_id=self.task_id)
+        job_status_api_url = f"{self.base_url}/api/v1/dags/{self.dag_id}/dagRuns/{self.dag_run_id}/taskInstances/{self.task_id}"
         # 请求中加入Authorization
         response = requests.get(job_status_api_url, headers={'Authorization':'Basic YWlyZmxvdzphaXJmbG93'})
-        response.raise_for_status()
-        print(response.json()['state'])
-        return response.json()['state']
+
+        try:
+            response.raise_for_status()
+        except requests.exceptions.HTTPError as e:
+            logging.error(f"Error occurred while fetching task status. Response code: {response.status_code}. Error message: {e}")
+
+        task_state = response.json()['state']
+        logging.info(f"Task state for task id {self.task_id} is {task_state}")
+        return task_state
 
     def check_dependencies(self):
         count = 0
