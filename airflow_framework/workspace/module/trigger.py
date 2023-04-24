ea293d49bb99801daab116e4ad4a2eede493251f
Nikkicat6 <127483949+Nikkicat6@users.noreply.github.com>
2023-04-24 15:15:56 +0800

    Update trigger.py

--- airflow_framework/workspace/module/trigger.py
+++ airflow_framework/workspace/module/trigger.py
@@ -3,10 +3,10 @@ import json
 from pprint import pprint
 from datetime import datetime
 import sched, time
-#获取当前时间
-def get_execution_time():
+
+def get_execution_time(): #获取当前时间
     # datetime object containing current date and time
-    now = datetime.utcnow()
+    now = datetime.utcnow()#获取当前世界时间
 
     print("now =", now)
 
@@ -15,7 +15,7 @@ def get_execution_time():
 
     return dt_string
 
-dag_id = "cedc_airflow_id"#写死一个dag_id
+dag_id = "cedc_airflow_dag_id"#写死一个dag_id
 
 def trigger_dag():
     exec_time = get_execution_time()
@@ -31,10 +31,10 @@ def trigger_dag():
     header = {"content-type": "application/json"}
 
     result = requests.post(
-        url="",#API接口
+        url=" ",
         data=json.dumps(data),
         headers=header,
-        auth=("admin", "admin"))#用户名密码
+        auth=("admin", "admin"))
 
     pprint(result.content.decode('utf-8'))
 
@@ -44,3 +44,34 @@ def trigger_dag():
 
     return result
 
+
+def get_dag_run(dag_run_id):
+    result = requests.get(
+        url=" ",
+        auth=("admin", "admin"))
+
+    pprint(result.content.decode('utf-8'))
+
+
+    result = json.loads(result.content.decode('utf-8'))
+
+    pprint(result)
+
+    return result
+
+result = trigger_dag()
+dag_run_id = result["dag_run_id"]
+
+s = sched.scheduler(time.time, time.sleep)#调度时间
+"""
+def watch_dag_until_complete():
+    result = get_dag_run(dag_run_id)
+    state = result["state"]
+
+    if state != "success":
+        s.enter(1, 1, watch_dag_until_complete)
+    else:
+        print("dag completed!")
+
+s.enter(1, 1, watch_dag_until_complete)"""
+s.run()
