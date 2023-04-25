04f5e6edf2b06cf789ecb3da71c1ecd773ecfb0e
LUYAN <luyanlovely@163.com>
2023-04-25 17:02:15 +0800

    新建start test 测试文件

---
+++ tests/test_start.py
@@ -0,0 +1,10 @@
+import unittest
+from airflow_framework.workspace.module import start
+
+class TestStart(unittest.TestCase):
+    def test_start(self):
+        event = {"datasource_name": "sample",
+                 "load_type": "ALL",
+                 "run_type": "glue",
+                 "glue_template_name": "devops.prelanding.s3_file_movement"}
+        self.assertEqual(None, start.Start().run(event))  # add assertion here
