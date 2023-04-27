f741e3542389fde521033b27b605e51988946bfe
Julie Wang <127485375+julie7007@users.noreply.github.com>
2023-04-27 12:22:19 +0800

    通用方法，存放不同模块所对应的方法

---
+++ airflow_framework/workspace/module/batch_processor.py
@@ -0,0 +1,46 @@
+from module.monitor import Monitor
+from module.start import Start
+from module.notify import Notify
+from module.dependency import Dependency
+from module.trigger import Trigger
+
+"""
+    公用方法
+    存放不同模块调用所对应的方法
+    """
+def start_batch(event):
+    try:
+        Start().run(event)
+        return "Successfully run the batch"
+    except Exception as e:
+        # insrt log here
+        exit(1)
+
+def monitor_batch(event):
+    try:
+        Monitor().monitor(event)
+        return "Completed monitoring"
+    except Exception as e:
+        # insrt log here
+        exit(1)
+def batch_notify(event):
+    try:
+        Notify().send_job_result(event)
+        return "Successfully send notification"
+    except Exception as e:
+        # insrt log here
+        exit(1)
+def dependency_check(event):
+    try:
+        Dependency().get_dependency_status(event)
+        return "Completed dependency checking"
+    except Exception as e:
+        # insrt log here
+        exit(1)
+def trigger_next_dag(event):
+    try:
+        Trigger().trigger_dag(event)
+        return "Successfully trigger next dag"
+    except Exception as e:
+        # insrt log here
+        exit(1)
