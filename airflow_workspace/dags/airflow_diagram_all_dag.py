b224658f59ff3fc1268771c2a969f8e1914021ad
Julie Wang <127485375+julie7007@users.noreply.github.com>
2023-05-09 17:23:38 +0800

    Create airflow_diagram_all_dag.py
    调用所有dag生成血缘关系

---
+++ airflow_workspace/dags/airflow_diagram_all_dag.py
@@ -0,0 +1,102 @@
+# -*- coding: utf-8 -*-
+"""
+@Desc : ALL DAGs relationship diagram
+@Update: YYYY/MM/DD author description
+2023/5/9 Julie initial version
+"""
+################################################################################################
+# import
+################################################################################################
+import json
+from datetime import datetime, timedelta, time
+from airflow.exceptions import AirflowSkipException, AirflowFailException
+from airflow.operators.python import PythonOperator
+from airflow.models.baseoperator import chain
+from airflow import DAG
+from airflow.operators.trigger_dagrun import TriggerDagRunOperator
+import os
+# from airflow_workspace.utils.postgre_handler import PostgresHandler
+
+################################################################################################
+# These args will get passed on to each operator
+################################################################################################
+
+
+default_args = {
+    'owner': 'cedc_airflow',
+    'depends_on_past': False,
+    'start_date': datetime(2023, 4, 21),
+    'email': ['julie.wang@cognizant.com'],
+    'email_on_failure': True,
+    'email_on_retry': True,
+    'email_on_success': True,
+    'retries': 0
+}
+################################################################################################
+# Dag List
+################################################################################################
+
+dag_list = [{'dag_name': 'a', 'flag': 'success'},
+            {'dag_name': 'b', 'flag': 'failed'},
+            {'dag_name': 'c', 'flag': 'success'},
+            {'dag_name': 'd', 'flag': 'success'},
+            {'dag_name': 'e', 'flag': None},
+            {'dag_name': 'f', 'flag': 'success'}
+            ]
+
+
+################################################################################################
+# Check DAG status
+################################################################################################
+def check_dag_status(dag_name, flag, **context):
+    # """
+    # :param dag_name: dag id
+    # :param flag: dag再数据库里执行成功还是失败
+    # :return:
+    # """
+    run_id = os.environ["AIRFLOW_CTX_DAG_RUN_ID"]
+    is_manual = run_id.startswith('manual__')
+    # WIP
+    if is_manual:
+        TriggerDagRunOperator(
+            task_id='start',
+            trigger_dag_id=dag_name
+        ).execute(context)
+    else:
+        if len(flag) == 0:
+            raise AirflowSkipException
+        elif flag == 'success':
+            pass
+        elif flag == 'failed':
+            raise AirflowFailException
+
+
+with DAG(
+        dag_id="dag_diagram",
+        schedule_interval='15 * * * 1-5',
+        catchup=False,
+        tags=["sales"],
+        default_args=default_args
+) as dag:
+    names = locals()
+    for i, element in enumerate(dag_list):
+        names['%s' % element['dag_name']] = PythonOperator(
+            task_id=f"{element['dag_name']}",
+            python_callable=check_dag_status,
+            op_kwargs={"dag_name": element['dag_name'], "flag": element['flag']},
+            dag=dag
+        )
+
+    ################################################################################################
+    # Get dag dependency
+    # PostgresHandler().get_record
+    # ret_json = [[a,b],[e,f],[a,d]]
+    ################################################################################################
+
+    # Query_SQL = """ SELECT DAG_NAME,DEPENDENCY_DAG_NAME FROM dag_dependence WHERE TAGS = '{sales}' """
+    # result_json = json.loads(PostgresHandler().get_record(Query_SQL))
+    result_json = [['a','b'],['e','f'],['a','d']]
+    # parent_task = None
+
+    # for j, element in enumerate(result_json):
+    chain(result_json[0])
