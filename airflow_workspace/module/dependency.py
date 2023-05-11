efd16cb2359457f85ab429324d7afc8fb7bf7dfd
liuzhu <62790503+Untitled1988@users.noreply.github.com>
2023-05-11 07:34:38 +0000

    dependency check v2

--- airflow_workspace/module/dependency.py
+++ airflow_workspace/module/dependency.py
@@ -9,9 +9,11 @@ from datetime import datetime, timedelta
 from airflow.exceptions import AirflowFailException  # make the task failed without retry
 from airflow.exceptions import AirflowException  # failed with retry
 from airflow_workspace.utils.logger_handler import logger
-from airflow_workspace.utils.airflow_api_handler import AirflowAPIHandler
+from airflow_workspace.utils.airflow_dag_handler import AirflowDagHandler
+from airflow_workspace.utils.postgre_handler import PostgresHandler
 
 logger = logger()
+
 
 class Dependency:
     """
@@ -32,43 +34,48 @@ class Dependency:
         self.max_waiting_count = 3
         self.base_url = 'http://43.143.250.12:8080'
 
+    
+
     def check_dependencies(self,event):
-        self.event = event
         
-        self.dag_ids = event['dag_ids']
+        self.event = event
+
+        self.dag_id = event['dag_id']  # 解析出main传过来的dag_id
         # self.execution_date = event['execution_date']
         self.waiting_time = event['waiting_time']
         self.max_waiting_count = event['max_waiting_count']
         self.base_url = event['base_url']
-
-        api_utils = AirflowAPIHandler(self.base_url)
+        dag_handler = AirflowDagHandler(self.base_url)
+        
+        self.dag_ids = dag_handler.get_dependencies_dag_ids_by_db(self.dag_id)  # 通过dag_id获取依赖的dag_ids列表
 
         for dag_id in self.dag_ids:
             count = 0
             while True:
-                status = api_utils.get_dag_status_by_api(dag_id)
-                logger.info(f"Current DAG status for dag id {dag_id}: {status}")
-                if status == 'success':
-                    logger.info(f'任务{dag_id}check成功,状态为 {status}')
+                state = dag_handler.get_dag_state_by_api(dag_id)
+                logger.info(f"Current DAG state for dag id {dag_id}: {state}")
+                if state == 'success':
+                    logger.info(f'任务{dag_id}check成功,状态为 {state}')
                     break
-                elif status in {'queued', 'running'}:
+                elif state in {'queued', 'running'}:
                     count += 1
                     if count >= self.max_waiting_count:
                         logger.info(f'任务等待时间过长，已等待 {self.max_waiting_count * self.waiting_time} 秒')
                         raise AirflowFailException(f'任务等待时间过长，已等待 {self.max_waiting_count * self.waiting_time} 秒')
                 else:
-                    logger.info(f'任务{dag_id}check失败，状态为 {status}')
-                    raise AirflowFailException(f'任务{dag_id}check失败，状态为 {status}')
+                    logger.info(f'任务{dag_id}check失败，状态为 {state}')
+                    raise AirflowFailException(f'任务{dag_id}check失败，状态为 {state}')
                 time.sleep(self.waiting_time)
 
 if __name__ == '__main__':
 
     checker = Dependency()
-    event = {"dag_ids": ["first_dag", "second_dag"],
+    event = {"dag_id": "dag_cedc_sales_pub3",
             # "execution_date": datetime(2023, 4, 26),
              "waiting_time": 4,
              "max_waiting_count": 2,
              "base_url" : "http://43.143.250.12:8080"
         }
     checker.check_dependencies(event)
+
 
