9d614cfd6e1cb64bc330ba6bf589703da084cd71
CyberXie <xieyime@outlook.com>
2023-05-16 10:16:58 +0800

    parameters validation 2023/5/16 updated

--- airflow_workspace/utils/params_validator.py
+++ airflow_workspace/utils/params_validator.py
@@ -6,13 +6,14 @@
 
 from airflow.exceptions import AirflowException
 
-"""
-dependency 参数验证
-Parameters validator for dependency
-"""
+
 def dependency_validator(event):
-    dag_ids = event['dag_ids']
-    execution_date = event['execution_date']
+    """
+    dependency 参数验证
+    Parameters validator for dependency
+    """
+    dag_id = event['dag_id']
+    # execution_date = event['execution_date']
     waiting_time = event['waiting_time']
     max_waiting_count = event['max_waiting_count']
     base_url = event['base_url']
@@ -21,20 +22,26 @@ def dependency_validator(event):
     判断参数是否为空
     Empty validator for parameters
     """
-    params = [dag_ids, execution_date, waiting_time, max_waiting_count, base_url]
+    params = [dag_id, waiting_time, max_waiting_count, base_url]
     for param in params:
         if not param:
             raise AirflowException("missing argument : {}".format(param))
 
 
-"""
-monitor 参数验证
-Parameters validator for monitor
-"""
 def monitor_validator(event):
+    """
+    monitor 参数验证
+    Parameters validator for monitor
+    """
     datasource_name = event['datasource_name']
     load_type = event['load_type']
     run_type = event['run_type']
+    glue_template = event['glue_template_name']
+
+    # run_type为glue时，验证glue template是否存在
+    # Validating the glue template while run_type == 'glue'
+    if run_type == 'glue' and not glue_template:
+        raise AirflowException("Glue template name missing")
 
     '''
     判断任意一个参数是否为空，为空抛出AirflowException并停止
@@ -53,11 +60,11 @@ def monitor_validator(event):
         raise AirflowException("Wrong Type : run_type")
 
 
-"""
-notify 参数验证
-Parameters validator for notify
-"""
 def notify_validator(event):
+    """
+    notify 参数验证
+    Parameters validator for notify
+    """
     job_name = event['datasource_name']
 
     '''
@@ -68,28 +75,40 @@ def notify_validator(event):
         raise AirflowException("missing argument : job_name")
 
 
-"""
-start 参数验证
-Parameters validator for start
-"""
 def start_validator(event):
     """
-    原本python代码中含有参数验证模块
-    parameters will be validated in the original python scripts
+    start 参数验证
+    Parameters validator for start
     """
-    pass
+    datasource_name = event['datasource_name']
+    load_type = event['load_type']
+    glue_template = event['glue_template_name']
+
+    """
+    判断任意一个参数是否为空，为空抛出AirflowException并停止
+    throw AirflowException and stop itself while one of the parameters is empty
+    源代码中含有run_type参数验证模块
+    run_type have already been validated in the original python script.
+    """
+    params = [datasource_name, load_type, glue_template]
+    for param in params:
+        if not param:
+            raise AirflowException("missing argument : {}".format(param))
 
 
-"""
-trigger 参数验证
-Parameters validator for trigger
-"""
 def trigger_validator(event):
+    """
+    trigger 参数验证
+    Parameters validator for trigger
+    """
     dag_run_id = event['dag_run_id']
+    url = event['url']
+    array_list = [dag_run_id, url]
 
     """
     判断任意一个参数是否为空，为空抛出AirflowException并停止
     throw AirflowException and stop itself while one of the parameters is empty
     """
-    if not dag_run_id:
-        raise AirflowException("missing argument dag_run_id")
+    for item in array_list:
+        if not item:
+            raise AirflowException("missing argument dag_run_id")
