fc886630b4b9f5a0e8a8e41ed9220ce27ec3d6a7
Hao Jing <127476363+Haohao2023@users.noreply.github.com>
2023-05-17 13:10:47 +0800

    Update trigger.py

--- airflow_workspace/module/trigger.py
+++ airflow_workspace/module/trigger.py
@@ -1,59 +1,48 @@
 import requests
 import json
+from airflow_workspace.utils.hash_handler import gen_random_hasher
 from airflow_workspace.utils.postgre_handler import PostgresHandler
 
 
-# http://54.65.139.140:8080
-
 class Trigger:
-    # param dag_id: 通过main获取dag_id
+    # 调用下一个Dag
+    # dag_run_id: 通过随机数获取
 
     def __init__(self):
-        self.url = 'http://54.65.139.140:8080'
         self.dag_id = ''
+        self.url = 'http://18.183.162.78'
 
     def trigger_next_dag(self, event):
-
-        self.url = event['url']
         self.dag_id = event['dag_id']
+        self.url = event['url']
+
+        run_id = gen_random_hasher()
 
         pg_handler = PostgresHandler()
 
         # param dag_run_id: 根据main传入的'dag_id'在数据库中查找对应的dag_run_id
-        sql_get_dag = f"select run_id from fact_dag_details WHERE dag_id = event['dag_id']"
-        run_id = pg_handler.get_record(sql_get_dag)
-        dag_run_id = run_id[0]['run_id']
+        sql_get_dag = f"select dag_name from dim_dag WHERE dag_name = {self.dag_id} and is_active = 'Y'"
+        get_dag = pg_handler.get_record(sql_get_dag)
 
-        header = {'Authorization': 'Basic YWlyZmxvdzphaXJmbG93',
-                  'Content-Type': 'application/json'}
-        body = {
-            "dag_run_id": dag_run_id
-        }
+        if not get_dag:
+            print("引发异常：dag_name 为空或不存在")
+        else:
+            header = {'Authorization': 'Basic YWlyZmxvdzphaXJmbG93',
+                      'Content-Type': 'application/json'}
 
-        result = requests.get(
-            f"{self.url}/api/v1/dags/{self.dag_id}/details",
-            headers=header
-        )
+            body = {
+                "dag_run_id": run_id
+            }
 
-        # 判断airflow中dag是否存在，存在即调用，不存在返回错误信息
-        try:
-            dag_result = result.json()['dag_id']
-            if not dag_result:
-                raise KeyError("error")
-            else:
-                post = requests.post(
-                    f"{self.url}/api/v1/dags/{self.dag_id}/dagRuns",
-                    data=json.dumps(body),
-                    headers=header)
-                print(post.text)
-        except KeyError as e:
-            print("引发异常：", repr(e))
-
-        return result
+            post = requests.post(
+                f"{self.url}/api/v1/dags/{self.dag_id}/dagRuns",
+                data=json.dumps(body),
+                headers=header)
+            print(post.text)
 
 
 # if __name__ == "__main__":
-#     event = {"dag_id": "dataset_consumes_1",
-#              "url": "http://54.65.139.140:8080"
+#     event = {"dag_id": "dataset_consumes_145678889",
+#              "url": "http://18.183.162.78"
 #              }
 #     Trigger().trigger_next_dag(event)
