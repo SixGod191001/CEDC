256bdead95d2259689e78931e5e6c6a560a70cc0
linxinyu <131854300+SJQj@users.noreply.github.com>
2023-05-19 15:19:38 +0800

    Update test_email_handler.py

--- airflow_workspace/tests/test_email_handler.py
+++ airflow_workspace/tests/test_email_handler.py
@@ -6,11 +6,11 @@ class Testemail_handler(unittest.TestCase):
 
     def testsend_email_ses(self):
         check = email_handler.EmailHandler()
-        self.assertTrue(check.send_email_ses)
+        self.assertIsInstance(check.send_email_ses,bool)
 
     def testsend_email_sns(self):
         check = email_handler.EmailHandler()
-        self.assertTrue(check.send_email_sns)
+        self.assertIsInstance(check.send_email_sns,bool)
 
 if __name__ == "__main__":
     Testemail_handler.testsend_email_sns()
