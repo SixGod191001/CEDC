8ca170b8b10056822d51a77bf6ca216f2e8e01cc
ElaineXin <127581795+ElaineXin@users.noreply.github.com>
2023-05-26 16:23:41 +0800

    Update postgre_handler.py

--- airflow_workspace/utils/postgre_handler.py
+++ airflow_workspace/utils/postgre_handler.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+import time
+
 import psycopg2
 import json
 from airflow_workspace.utils.secrets_manager_handler import SecretsManagerSecret
@@ -6,6 +8,8 @@ from airflow_workspace.utils.secrets_manager_handler import SecretsManagerSecret
 from airflow.exceptions import AirflowFailException
 from airflow.exceptions import AirflowException  # failed with retry
 from airflow_workspace.utils import logger_handler
+import datetime
+from dbutils.pooled_db import PooledDB
 
 logger = logger_handler.logger()
 
@@ -16,6 +20,7 @@ class PostgresHandler:
         @desc: 从secret manager中获取到连接数据库的信息
         :param secret_manager_name: aws secret manager name
         """
+        self._pool = None
         sm_info = json.loads(SecretsManagerSecret().get_cache_value(secret_name=secret_manager_name))
         self.dataBaseName = sm_info['dbname']
         self.userName = sm_info['username']
@@ -29,30 +34,47 @@ class PostgresHandler:
             # 创建一个cursor  获取游标对象
             self._cur = self._conn.cursor()
 
-    # 获取数据库连接对象
-    def get_connect(self):
+    def init_pgsql_pool(self):
+        '''初始化连接池
+        '''
         try:
-            conn = psycopg2.connect(
-                database=self.dataBaseName,
+            logger.info('Begin to create {0} postgresql pool on：{1}.\n'.format(self.host, datetime.datetime.now()))
+            pool = PooledDB(
+                creator=psycopg2,  # 使用连接数据库的模块 psycopg2
+                maxconnections=6,  # 连接池允许的最大连接数，0 和 None 表示不限制连接数
+                mincached=1,  # 初始化时，链接池中至少创建的空闲的链接，0 表示不创建
+                maxcached=2,  # 链接池中最多闲置的链接，0 和 None 不限制
+                blocking=True,  # 连接池中如果没有可用连接后，是否阻塞等待。True，等待；False，不等待然后报错
+                maxusage=None,  # 一个链接最多被重复使用的次数，None 表示无限制
+                setsession=[],  # 开始会话前执行的命令列表
+                host=self.host,
+                port=self.port,
                 user=self.userName,
                 password=self.password,
-                host=self.host,
-                port=self.port
-            )
-        except Exception as err:
-            logger.error("连接数据库失败，%s" % err)
-            raise AirflowException("get connect is bad!")
-        return conn
+                database=self.dataBaseName)
+            self._pool = pool
+            logger.info(
+                'SUCCESS: create {0} postgresql pool success on {1}.\n'.format(self.host, datetime.datetime.now()))
+
+        except Exception as e:
+            logger.error('ERROR: create {0} postgresql pool failed on {1}.\n'.format(self.host, datetime.datetime.now()))
+            raise AirflowException('ERROR: create postgresql pool error caused by {0}'.format(str(e)))
+
+    # 获取数据库连接对象
+    def get_connect(self):
+
+        if not self._pool:
+            self.init_pgsql_pool()
+        return self._pool.connection()
 
     def get_table_columns(self, table_name):
         try:
             query = """
-                    SELECT column_name
-                    FROM information_schema.columns
-                    WHERE table_name = %s
-                        AND (column_default NOT LIKE 'nextval%' OR column_default IS NULL)
-                    ORDER BY ordinal_position;
-                    """
+            SELECT column_name
+            FROM information_schema.columns
+            WHERE table_name = %s
+            ORDER BY ordinal_position;
+            """
             self._cur.execute(query, (table_name,))
             columns = [column[0] for column in self._cur.fetchall()]
             return columns
@@ -61,7 +83,7 @@ class PostgresHandler:
             raise AirflowException("获取表列信息失败！")
         finally:
             self._cur.close()
-            self._conn.close()
+            # self._conn.close()
 
     # 执行查询sql
 
@@ -70,6 +92,7 @@ class PostgresHandler:
         :param sql: 查看数据的自定义的sql
         :return: dict_results = [{"column name":"value","column name":"value"},{"column name":"value","column name":"value"}]
         """
+
         res = ""
         try:
             self._cur.execute(sql)
@@ -87,7 +110,7 @@ class PostgresHandler:
             raise AirflowException("get record is bad!")
         else:
             self._cur.close()
-            self._conn.close()
+            # self._conn.close()
             return dict_results
 
     # 执行insert
@@ -99,10 +122,10 @@ class PostgresHandler:
         :return: 当没有数据insert的时候会返回 9 ，insert成功时返回 0， 失败时返回 1
         """
         insert_sql = """ INSERT INTO FACT_JOB_DETAILS 
-                         (DAG_ID,TASK_ID,JOB_ID,RUN_ID,JOB_START_DATE,JOB_END_DATE,JOB_STATUS,INSERT_DATE,LAST_UPDATE_DATE)
-                         SELECT DAG.DAG_ID 
-                               ,TASK.TASK_ID 
-                               ,JOB.JOB_ID 
+                         (DAG_NAME,TASK_NAME,JOB_NAME,RUN_ID,JOB_START_DATE,JOB_END_DATE,JOB_STATUS,INSERT_DATE,LAST_UPDATE_DATE)
+                         SELECT DAG.DAG_NAME 
+                               ,TASK.TASK_NAME 
+                               ,JOB.JOB_NAME 
                                ,'{p_run_id}' AS RUN_ID 
                                ,CURRENT_TIMESTAMP AS JOB_START_DATE 
                                ,NULL AS JOB_END_DATE 
@@ -114,6 +137,7 @@ class PostgresHandler:
                          INNER JOIN DIM_DAG DAG ON TASK.DAG_NAME=DAG.DAG_NAME 
                          WHERE JOB.JOB_NAME='{p_job_name}' """
         sql = insert_sql.format(p_run_id=run_id, p_job_name=job_name, p_status=status)
+
         try:
             self._cur.execute(sql)
             self._conn.commit()
@@ -129,7 +153,7 @@ class PostgresHandler:
             raise AirflowException("execute_insert is bad!")
         else:
             self._cur.close()
-            self._conn.close()
+            # self._conn.close()
             return flag
 
     # 执行update
@@ -143,6 +167,7 @@ class PostgresHandler:
         update_sql = """UPDATE FACT_JOB_DETAILS SET JOB_END_DATE = CURRENT_TIMESTAMP, JOB_STATUS='{p_status}',
         LAST_UPDATE_DATE=CURRENT_TIMESTAMP WHERE RUN_ID ='{p_run_id}' AND JOB_NAME = '{p_job_name}' """
         sql = update_sql.format(p_run_id=run_id, p_job_name=job_name, p_status=status)
+
         try:
             self._cur.execute(sql)
             self._conn.commit()
@@ -158,7 +183,7 @@ class PostgresHandler:
             raise AirflowException("execute_update is bad!")
         else:
             self._cur.close()
-            self._conn.close()
+            # self._conn.close()
             return flag
 
     # 执行delete
@@ -184,8 +209,37 @@ class PostgresHandler:
             raise AirflowException("execute_delete is bad!")
         else:
             self._cur.close()
-            self._conn.close()
+            # self._conn.close()
             return flag
+    def execute_sql(self, sql):
+        """
+        :param run_id: job对应的执行的id
+        :param job_name: job名字
+        :param status: job的执行状态
+        :return: 当没有数据update的时候会返回 9 ，update成功时返回 0， 失败时返回 1
+        """
+        try:
+            self._cur.execute(sql)
+            self._conn.commit()
+            rowcount = self._cur.rowcount
+            if rowcount >= 1:
+                flag = 0
+            else:
+                flag = 9
+        except Exception as err:
+            flag = 1
+            self._conn.rollback()
+            logger.error("执行失败, %s" % err)
+            raise AirflowException("execute_update is bad!")
+        else:
+            self._cur.close()
+            # self._conn.close()
+            return flag
+    def close_pool(self):
+        '''关闭 pool
+        '''
+        if self._pool != None:
+            self._pool.close()
 
 
 if __name__ == "__main__":
@@ -194,12 +248,15 @@ if __name__ == "__main__":
     status = "running"
     conn = PostgresHandler()
     # response = conn.execute_insert(run_id=run_id, job_name=job_name, status=status)
-    # response = conn.execute_update(ded=run_id, job_name=job_name, status=status)
+    # response = conn.execute_update(run_id=run_id, job_name=job_name, status=status)
     # response = conn.execute_delete(run_id=run_id)
     # logger.info(response)
+    # update_sql = """UPDATE FACT_JOB_DETAILS SET JOB_END_DATE = CURRENT_TIMESTAMP WHERE id = 1 """
+    # response11 = conn.execute_sql(update_sql)
+    # print(response)
     # 查看查询结果
     Query_SQL = """ SELECT * FROM FACT_JOB_DETAILS WHERE RUN_ID = '{p_run_id}' """
     rows = conn.get_record(Query_SQL.format(p_run_id=run_id))
     print(rows)
-    # for row in rows:
-    #     logger.info(row)
+    response = conn.execute_update(run_id=run_id, job_name=job_name, status=status)
+    print(response)
