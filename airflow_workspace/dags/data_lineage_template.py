83527a3f12fcc291b762cd9b1257d3916f81ca42
luyan <luyanlovely@163.com>
2023-06-07 10:38:29 +0800

    update batch processor and tigger

--- airflow_workspace/dags/data_lineage_template.py
+++ airflow_workspace/dags/data_lineage_template.py
@@ -13,11 +13,12 @@ from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 import os
 from collections import Counter
 
-# from airflow_workspace.utils import postgre_handler
+from airflow_workspace.utils.postgre_handler import PostgresHandler
+
 
 # These args will get passed on to each operator
 default_args = {
-    'owner': 'cedc_airflow',
+    'owner': 'luyan',
     'depends_on_past': False,
     'start_date': datetime(2023, 4, 21),
     'email': ['julie.wang@cognizant.com'],
@@ -27,22 +28,58 @@ default_args = {
     'retries': 0
 }
 
-# 取状态
-# dag_list = postgre_handler.PostgresHandler.get_record("select * from xxx")
-dag_list = [{'dag_name': 'a', 'flag': 'success', 'tag': 'department1,dalian,sales'},
-            {'dag_name': 'b', 'flag': 'failed', 'tag': 'department2,beijing,sales'},
-            {'dag_name': 'c', 'flag': 'running', 'tag': 'department1,dalian,sales'},
-            {'dag_name': 'd', 'flag': 'success', 'tag': 'department1,dalian,sales'},
-            {'dag_name': 'e', 'flag': 'success', 'tag': 'department2,beijing,sales'},
-            {'dag_name': 'f', 'flag': 'success', 'tag': 'department2,beijing,sales'},
-            {'dag_name': 'g', 'flag': 'success', 'tag': 'department2,beijing,sales'},
-            {'dag_name': 'h', 'flag': 'success', 'tag': 'department3,shanghai,sales'},
-            {'dag_name': 'w', 'flag': 'success', 'tag': 'department3,shanghai,sales'},
-            ]
+def get_latest_dag_details():
+    # 取状态
+    sql = """
+    select dag_name,status as flag,tag
+    from 
+    (select f.dag_name,d.tag,f.status,execution_date,
+    row_number () over (partition by f.dag_name order by f.execution_date desc) idx
+    from 
+    public.fact_dag_details f
+    inner join public.dim_dag d on f.dag_name =d.dag_name 
+    )a where idx=1
+    """
+    db_handler = PostgresHandler()
+    dag_list = db_handler.get_record(sql)
+    # sample result
+    # dag_list = [{'dag_name': 'a', 'flag': 'success', 'tag': 'department1,dalian,sales'},
+    #             {'dag_name': 'b', 'flag': 'failed', 'tag': 'department2,beijing,sales'},
+    #             {'dag_name': 'c', 'flag': 'running', 'tag': 'department1,dalian,sales'},
+    #             {'dag_name': 'd', 'flag': 'success', 'tag': 'department1,dalian,sales'},
+    #             {'dag_name': 'e', 'flag': 'success', 'tag': 'department2,beijing,sales'},
+    #             {'dag_name': 'f', 'flag': 'success', 'tag': 'department2,beijing,sales'},
+    #             {'dag_name': 'g', 'flag': 'success', 'tag': 'department2,beijing,sales'},
+    #             {'dag_name': 'h', 'flag': 'success', 'tag': 'department3,shanghai,sales'},
+    #             {'dag_name': 'w', 'flag': 'success', 'tag': 'department3,shanghai,sales'},
+    #             ]
+    return dag_list
+
+query_dag_chains_sql = """
+    WITH RECURSIVE base AS (
+    SELECT dag_name,dag_name::varchar(500) as dag_chain 
+    FROM public.dim_dag_dependence
+    WHERE dependency_dag_name ='dag_cedc_start'
+    union
+    SELECT child.dag_name,(base.dag_chain||';'||child.dag_name)::varchar(500) as dag_chain
+    FROM public.dim_dag_dependence child
+    inner join base on  child.dependency_dag_name  = base.dag_name
+    )
+    SELECT replace(dag_chain,';dag_cedc_stop','') as dag_chain
+    FROM base
+    where dag_name ='dag_cedc_stop'
+    """
+
+dag_list=get_latest_dag_details()
 tag_list = sorted(list(set([str(tag) for dag in dag_list for tag in dag['tag'].split(',')])))
+# tag_list=sorted(list(set([dag['tag'] for dag in dag_list])))
 # 取父子关系
-# lst = postgre_handler.PostgresHandler.get_record("select * from xxx")
-lst = [["a", "b"], ["a", "c"], ["b", "d"], ["c", "e"], ["d", "f"], ["e", "f"], ["g", "h"], [None, "w"]]
+lst=[]
+dag_chain = PostgresHandler().get_record(query_dag_chains_sql)
+for i in [i['dag_chain'] for i in dag_chain ]:
+    ls=i.split(";")
+    lst.append(ls)
+# lst = [["a", "b"], ["a", "c"], ["b", "d"], ["c", "e"], ["d", "f"], ["e", "f"], ["g", "h"], [None, "w"]]
 
 
 def process(dag_name, flag, **context):
@@ -64,10 +101,10 @@ def process(dag_name, flag, **context):
 
 
 with DAG(
-        dag_id="data_lineage1",
+        dag_id="data_lineage2",
         schedule_interval='*/10 * * * 1-5',
         catchup=False,
-        tags=["sales", "dalian"],
+        tags=tag_list,
         default_args=default_args
 ) as dag:
     _ = locals()
