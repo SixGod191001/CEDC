31253681ff07e9e40b0a7120c951528ff03a175f
luyan <luyanlovely@163.com>
2023-06-08 15:05:12 +0800

    update data lineage

--- airflow_workspace/dags/data_lineage_template.py
+++ airflow_workspace/dags/data_lineage_template.py
@@ -13,6 +13,7 @@ from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 import os
 from collections import Counter
 
+from airflow_workspace.module.data_lineage import *
 from airflow_workspace.utils.postgre_handler import PostgresHandler
 
 
@@ -27,61 +28,12 @@ default_args = {
     'email_on_success': True,
     'retries': 0
 }
+tag= "'sales','dalian','department1'"
 
-def get_latest_dag_details():
-    # 取状态
-    sql = """
-    select dag_name,status as flag,tag
-    from 
-    (select f.dag_name,d.tag,f.status,execution_date,
-    row_number () over (partition by f.dag_name order by f.execution_date desc) idx
-    from 
-    public.fact_dag_details f
-    inner join public.dim_dag d on f.dag_name =d.dag_name 
-    )a where idx=1
-    """
-    db_handler = PostgresHandler()
-    dag_list = db_handler.get_record(sql)
-    # sample result
-    # dag_list = [{'dag_name': 'a', 'flag': 'success', 'tag': 'department1,dalian,sales'},
-    #             {'dag_name': 'b', 'flag': 'failed', 'tag': 'department2,beijing,sales'},
-    #             {'dag_name': 'c', 'flag': 'running', 'tag': 'department1,dalian,sales'},
-    #             {'dag_name': 'd', 'flag': 'success', 'tag': 'department1,dalian,sales'},
-    #             {'dag_name': 'e', 'flag': 'success', 'tag': 'department2,beijing,sales'},
-    #             {'dag_name': 'f', 'flag': 'success', 'tag': 'department2,beijing,sales'},
-    #             {'dag_name': 'g', 'flag': 'success', 'tag': 'department2,beijing,sales'},
-    #             {'dag_name': 'h', 'flag': 'success', 'tag': 'department3,shanghai,sales'},
-    #             {'dag_name': 'w', 'flag': 'success', 'tag': 'department3,shanghai,sales'},
-    #             ]
-    return dag_list
-
-query_dag_chains_sql = """
-    WITH RECURSIVE base AS (
-    SELECT dag_name,dag_name::varchar(500) as dag_chain 
-    FROM public.dim_dag_dependence
-    WHERE dependency_dag_name ='dag_cedc_start'
-    union
-    SELECT child.dag_name,(base.dag_chain||';'||child.dag_name)::varchar(500) as dag_chain
-    FROM public.dim_dag_dependence child
-    inner join base on  child.dependency_dag_name  = base.dag_name
-    )
-    SELECT replace(dag_chain,';dag_cedc_stop','') as dag_chain
-    FROM base
-    where dag_name ='dag_cedc_stop'
-    """
-
-dag_list=get_latest_dag_details()
-tag_list = sorted(list(set([str(tag) for dag in dag_list for tag in dag['tag'].split(',')])))
-# tag_list=sorted(list(set([dag['tag'] for dag in dag_list])))
-# 取父子关系
-lst=[]
-dag_chain = PostgresHandler().get_record(query_dag_chains_sql)
-for i in [i['dag_chain'] for i in dag_chain ]:
-    ls=i.split(";")
-    lst.append(ls)
-# lst = [["a", "b"], ["a", "c"], ["b", "d"], ["c", "e"], ["d", "f"], ["e", "f"], ["g", "h"], [None, "w"]]
-
-
+dag_list=get_dag_list(tag)
+lst=get_dag_chain(tag)
+# tag_list = sorted(list(set([str(tag) for dag in dag_list for tag in dag['tag'].split(',')])))
+tag_list=tag.replace("'",'').split(",")
 def process(dag_name, flag, **context):
     is_manual = os.environ["AIRFLOW_CTX_DAG_RUN_ID"].startswith('manual__')
     ti = context['ti']
@@ -91,9 +43,9 @@ def process(dag_name, flag, **context):
         if len(flag) == 0:
             ti.set_state(State.NONE)
             raise AirflowSkipException
-        elif flag == 'failed':
+        elif flag.lower() == 'failed':
             raise AirflowFailException
-        elif flag == 'running':
+        elif flag.lower() == 'running':
             ti.set_state(State.RUNNING)
             raise AirflowSkipException
         else:
@@ -102,7 +54,7 @@ def process(dag_name, flag, **context):
 
 with DAG(
         dag_id="data_lineage2",
-        schedule_interval='*/10 * * * 1-5',
+        schedule='*/10 * * * 1-5',
         catchup=False,
         tags=tag_list,
         default_args=default_args
@@ -125,9 +77,10 @@ with DAG(
     for idx, tag in enumerate(tag_list):
         _['%s' % tag] = BashOperator(task_id=f"{tag}", bash_command=f'echo {tag}', dag=dag)
 
-    head, tail = list(set([x[0] for x in lst if x[0] is not None]) - set([y[1] for y in lst])), list(
-        set([x[1] for x in lst]) - set([y[0] for y in lst if y[0] is not None]))
-
+    # head, tail = list(set([x[0] for x in lst if x[0] is not None]) - set([y[-1] for y in lst])), list(
+    #     set([x[-1] for x in lst]) - set([y[0] for y in lst if y[0] is not None]))
+    head = [k[1] for k in lst if k[0] == 'dag_cedc_start']
+    tail = [k[0] for k in lst if k[1] == 'dag_cedc_stop']
     for tag in tag_list: start >> _.get(tag)
 
     for i in dag_list:
@@ -136,5 +89,9 @@ with DAG(
                 _.get(str(t)) >> _.get(i['dag_name'])
 
     for j in tail: _.get(j) >> stop
+    for k in lst:
+        if k[0] is None:
+            _.get(k[1]) >> stop
+        elif k[0] != 'dag_cedc_start' and k[1] != 'dag_cedc_stop':
+            _.get(k[0]) >> _.get(k[1])
 
-    for k in lst: _.get(k[1]) >> stop if k[0] is None else _.get(k[0]) >> _.get(k[1])
