de79954beed2b487561ba9ecb8c806dfdb43af0c
zhenzhen1029 <127726477+zhenzhen1029@users.noreply.github.com>
2023-06-09 11:20:24 +0800

    Update trigger.py

--- airflow_workspace/module/trigger.py
+++ airflow_workspace/module/trigger.py
@@ -19,34 +19,36 @@ class Trigger:
         self.dag_id = event['dag_id']
         self.url = event['base_url']
         print(self.dag_id)
-
+        random.seed()
         run_id = random.randint(10000001, 19999999)
 
         pg_handler = PostgresHandler()
 
         # param dag_run_id: 根据main传入的'dag_id'在数据库中查找对应的dag_run_id
-        sql_get_dag = f"select dag_name from dim_dag WHERE dag_name = '{self.dag_id}' and is_active = 'Y'"
-        get_dag = pg_handler.get_record(sql_get_dag)
-        print(get_dag)
-        if not get_dag:
-            print("引发异常：dag_name 为空或不存在")
-        else:
-            header = {'Authorization': 'Basic YWlyZmxvdzphaXJmbG93',
-                      'Content-Type': 'application/json'}
+        sql_get_dags = f"select dag_name from dim_dag_dependence WHERE dependency_dag_name = '{self.dag_id}' and is_active = 'Y'"
+        get_dags = pg_handler.get_record(sql_get_dags)
 
-            body = {
-                "dag_run_id": run_id
-            }
+        for i in get_dags:
+            values = list(i.values())
+            dag_name = values[0]
+            if not dag_name:
+                print("引发异常：dag_name 为空或不存在")
+            else:
+                header = {'Authorization': 'Basic YWlyZmxvdzphaXJmbG93',
+                          'Content-Type': 'application/json'}
 
+                body = {
+                    "dag_run_id": str(run_id)
+                }
             post = requests.post(
-                f"{self.url}/api/v1/dags/{self.dag_id}/dagRuns",
+                f"{self.url}/api/v1/dags/{dag_name}/dagRuns",
                 data=json.dumps(body),
                 headers=header)
             print(post.text)
 
 
-# if __name__ == "__main__":
-#     event = {"dag_id": "dataset_consumes_145678889",
-#              "base_url": "http://43.143.250.12:8080"
-#              }
-#     Trigger().trigger_next_dag(event)
+if __name__ == "__main__":
+    event = {"dag_id": "dag_cedc_sales_a",
+             "base_url": "http://43.143.250.12:8080"
+             }
+    Trigger().trigger_next_dag(event)
