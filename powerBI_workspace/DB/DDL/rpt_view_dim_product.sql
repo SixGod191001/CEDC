a6e867dcb46103fe608cfc214a4025b752d1c29f
faraxie <132861910+faraxie@users.noreply.github.com>
2023-06-16 11:38:25 +0800

    Delete rpt_view_dim_product.sql

--- powerBI_workspace/DB/DDL/rpt_view_dim_product.sql
+++
@@ -1,3 +0,0 @@
-CREATE VIEW apdb.rpt_view_dim_product AS
-select Distinct BrandCode,BrandDesc from apdb.dim_brand 
-where BrandCode in (select Distinct BrandCode from apdb.fact_ims_city union select Distinct BrandCode from apdb.fact_ims_chpa);
