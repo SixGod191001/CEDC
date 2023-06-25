947dc50587dbfa5052839753cd5c7e95cbb4c16d
luyan <luyanlovely@163.com>
2023-06-25 18:33:49 +0800

    update the logic to process current_timestamp

--- airflow_workspace/metadata_db/DML/json_publish_db.py
+++ airflow_workspace/metadata_db/DML/json_publish_db.py
@@ -59,7 +59,7 @@ class PublishData:
             values = []
             for row in json_table_data:
                 row_values = ', '.join(f"'{value}'" for value in row.values())
-                row_values = row_values.lower().replace("'current_timestamp'", "current_timestamp")
+                row_values = row_values.replace("'current_timestamp'", "current_timestamp")
                 values.append(f"({row_values})")
             
             insert_query += ', '.join(values)
